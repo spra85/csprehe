206c2a685c83329f805b10711d7a54f729ddc985
Chris Sprehe <chris.sprehe@gmail.com>
2014-01-25 16:51:38 -0600

    added some styling; messing around with mouseenter/mouseleave

--- assets/javascript/views/application_view.js
+++ assets/javascript/views/application_view.js
@@ -1,3 +1,17 @@
 App.ApplicationView = Ember.View.extend({
-  classNames: ['application']
+  changeDescription: function(event) {
+    var target = $(event.target).closest("a");
+    this.get("controller").set("activeLinkDescription", target.data("desc"));
+  },
+
+  clearDescription: function() {
+    this.get("controller").set("activeLinkDescription", null);
+  },
+
+  setupEvents: function() {
+    _.bindAll(this, "changeDescription", "clearDescription");
+    var iconsContainer = $(".icons");
+    iconsContainer.on("mouseenter", ".nav-link", this.changeDescription);
+    iconsContainer.on("mouseleave", ".nav-link", this.clearDescription);
+  }.on("didInsertElement")
 });
