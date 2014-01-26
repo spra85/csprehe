62151285d22a37340afd0563b6247f6c31bac21f
Chris Sprehe <chris.sprehe@gmail.com>
2014-01-26 10:28:47 -0600

    used dynamic background loading for pixel ratio

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -8,3 +8,17 @@ App = Ember.Application.create({
 App.ApplicationAdapter = DS.FixtureAdapter;
 App.deferReadiness();
 App.Map = {};
+
+function loadBackgroundImage() {
+  var $el = $("#container");
+  console.log("loadBackgroundImage");
+  var src;
+  if (window.devicePixelRatio && window.devicePixelRatio > 1) {
+    src = $el.data("x2-src");
+  } else {
+    src = $el.data("src");
+  }
+  $el.css("background-image", "url(" + src + ")");
+}
+
+$(window).on("DOMContentLoaded load", loadBackgroundImage);
