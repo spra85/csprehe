2f540fb7f28f3bdf9690edf910a888337cdd40c9
Chris Sprehe <chris.sprehe@gmail.com>
2014-02-16 09:12:35 -0600

    custom marker pin

--- assets/javascript/views/map_view.js
+++ assets/javascript/views/map_view.js
@@ -1,5 +1,18 @@
 L.Icon.Default.imagePath = "images";
 
+var customPin = L.icon({
+  iconUrl: "images/custom_pin.png",
+  iconSize: [10, 35],
+  iconAnchor: [5, 35]
+});
+
+EmberLeaflet.MarkerLayer.reopen({
+  init: function() {
+    this._super();
+    this.set("options", { icon: customPin });
+  }
+});
+
 App.MarkerCollectionLayer = EmberLeaflet.MarkerCollectionLayer.extend({
   contentBinding: "controller"
 });
